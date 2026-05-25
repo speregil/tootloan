13a7af4a6937576fbb8b7249049f28da6cf74138
Sebastian <s.gilparga@gmail.com>
2026-05-25 16:12:19 +1200

    First Commit

---
+++ ToolLoan/ToolLoan/Library.cs
@@ -0,0 +1,15 @@
+namespace ToolLoan
+{
+    public partial class Library : Form
+    {
+        public Library()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
